ef56151eba2a3f06eeb549457c62c0cf4060f5f3
Alireza Esfahani <aynzad@gmail.com>
2021-01-24 16:09:10 +0330

    ✅ WIP: add i18ns

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,10 +9,44 @@ export default {
         name: 'description',
         content: 'Alireza Esfahani website',
       },
+      { name: 'apple-mobile-web-app-title', content: 'Alireza Esfahani' },
+      { name: 'application-name', content: 'Alireza Esfahani' },
+      { name: 'msapplication-TileColor', content: '#C62641' },
+      { name: 'theme-color', content: '#C62641' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#C62641' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
+      },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@200..700',
+      },
+    ],
   },
-  loading: { color: '#fff' },
+  loading: { color: '#C62641' },
   buildModules: ['@nuxt/typescript-build'],
   typescript: {
     typeCheck: {
@@ -40,20 +74,40 @@ export default {
       detectBrowserLanguage: {
         useCookie: true,
         cookieKey: 'i18n_redirected',
-        onlyOnRoot: true, // recommended
+        onlyOnRoot: true,
       },
       messages: {
         en: {
-          alirezaEsfahani: 'Alireza Esfahani',
-          frontendDeveloper: 'Front-end developer',
-          switchLocale: 'فارسی',
-          articles: 'Articles',
+          logo: 'A',
+          home: {
+            title: 'Alireza Esfahani',
+            subtitle: 'The true story of my life as a',
+            job: 'Front-end developer',
+          },
+          menu: {
+            articles: 'Articles',
+            projects: 'Projects',
+            github: 'Github',
+            about: 'About',
+            contact: 'Contact',
+            switchLocale: 'فارسی',
+          },
         },
         fa: {
-          alirezaEsfahani: 'علیرضا اصفهانی',
-          frontendDeveloper: 'توسعه‌دهنده فرانت‌اند',
-          switchLocale: 'English',
-          articles: 'نوشته‌ها',
+          logo: 'ع',
+          home: {
+            title: 'علیرضا اصفهانی',
+            subtitle: 'داستان واقعی زندگی من به عنوان یک',
+            job: 'توسعه‌دهنده فرانت‌اند',
+          },
+          menu: {
+            articles: 'نوشته‌ها',
+            projects: 'پروژه‌ها',
+            github: 'گیت‌هاب',
+            about: 'درباره',
+            contact: 'ارتباط',
+            switchLocale: 'English',
+          },
         },
       },
     },
