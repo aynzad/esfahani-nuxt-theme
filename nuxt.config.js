b711f4a1be538f33e1504b310b267377246f43e9
Alireza Esfahani <aynzad@gmail.com>
2021-01-24 16:04:10 +0330

    🔴 WIP change nuxt config to check deploy issue

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,3 @@
-import { locales, messages } from './i18n';
 export default {
   head: {
     title: 'Alireza Esfahani',
@@ -10,44 +9,10 @@ export default {
         name: 'description',
         content: 'Alireza Esfahani website',
       },
-      { name: 'apple-mobile-web-app-title', content: 'Alireza Esfahani' },
-      { name: 'application-name', content: 'Alireza Esfahani' },
-      { name: 'msapplication-TileColor', content: '#C62641' },
-      { name: 'theme-color', content: '#C62641' },
     ],
-    link: [
-      {
-        rel: 'apple-touch-icon',
-        sizes: '180x180',
-        href: '/apple-touch-icon.png',
-      },
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '32x32',
-        href: '/favicon-32x32.png',
-      },
-      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#C62641' },
-      { rel: 'manifest', href: '/site.webmanifest' },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '16x16',
-        href: '/favicon-16x16.png',
-      },
-      {
-        rel: 'preconnect',
-        href: 'https://fonts.gstatic.com',
-      },
-      {
-        rel: 'stylesheet',
-        href:
-          'https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@200..700',
-      },
-    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
-  loading: { color: '#C62641' },
+  loading: { color: '#fff' },
   buildModules: ['@nuxt/typescript-build'],
   typescript: {
     typeCheck: {
@@ -59,16 +24,38 @@ export default {
   modules: ['nuxt-i18n'],
   i18n: {
     seo: true,
-    locales,
+    locales: [
+      {
+        code: 'en',
+        iso: 'en-US',
+      },
+      {
+        code: 'fa',
+        iso: 'fa-IR',
+      },
+    ],
     defaultLocale: 'en',
     vueI18n: {
       fallbackLocale: 'en',
       detectBrowserLanguage: {
         useCookie: true,
         cookieKey: 'i18n_redirected',
-        onlyOnRoot: true,
+        onlyOnRoot: true, // recommended
       },
-      messages,
+      messages: {
+        en: {
+          alirezaEsfahani: 'Alireza Esfahani',
+          frontendDeveloper: 'Front-end developer',
+          switchLocale: 'فارسی',
+          articles: 'Articles',
+        },
+        fa: {
+          alirezaEsfahani: 'علیرضا اصفهانی',
+          frontendDeveloper: 'توسعه‌دهنده فرانت‌اند',
+          switchLocale: 'English',
+          articles: 'نوشته‌ها',
+        },
+      },
     },
   },
   env: {
