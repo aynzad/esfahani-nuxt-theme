49c45f165a49764c6c84480f1363f5b7693a5711
Alireza Esfahani <aynzad@gmail.com>
2021-03-28 12:10:13 +0430

    ✨ Add pagination

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -1,13 +1,30 @@
 <template>
-  <div>
-    <section v-for="article in articles" :key="article.uid" class="articles">
-      <NuxtLink
-        :to="localePath({ name: 'articles-id', params: { id: article.uid } })"
+  <section class="container articles">
+    <section-title :title="$t('articles.title')" />
+    <article v-for="article in articles" :key="article.uid" class="article">
+      <article-title
+        :id="article.uid"
+        has-link
+        :title="getTitle(article)"
+        :subtitle="getSubtitle(article)"
+        :date="article.first_publication_date"
+        :tags="article.tags"
+        :read-time="article.data.read_time"
+      />
+    </article>
+    <footer>
+      <Button
+        v-if="articles.length > 0 && totalPages > page"
+        :loading="isLoadingMore"
+        @click="loadMore(page + 1)"
       >
-        {{ getTitle(article) }}
-      </NuxtLink>
-    </section>
-  </div>
+        {{ $t('loadMore') }}
+      </Button>
+      <div v-if="articles.length > 0 && totalPages === page" class="done">
+        &bull; &bull;
+      </div>
+    </footer>
+  </section>
 </template>
 
 <script lang="ts">
@@ -15,47 +32,110 @@ import Vue from 'vue';
 import Prismic from 'prismic-javascript';
 import PrismicDom from 'prismic-dom';
 import { Document } from 'prismic-javascript/types/documents';
-import { apiUrl } from '~/utils/config';
+import ResolvedApi from 'prismic-javascript/types/ResolvedApi';
+import ArticleTitle from '~/components/article/articleTitle/index.vue';
+import SectionTitle from '~/components/article/sectionTitle/index.vue';
+import Button from '~/components/core/Button.vue';
+import { apiUrl, pageSize } from '~/utils/config';
 
 export default Vue.extend({
   name: 'Articles',
+  components: {
+    ArticleTitle,
+    SectionTitle,
+    Button,
+  },
   async asyncData({ app }) {
     const lang = app.i18n.locale === 'en' ? 'en-us' : 'fa-ir';
     const api = await Prismic.getApi(apiUrl);
     const results = await api.query(
       Prismic.Predicates.at('document.type', 'articles'),
-      { lang }
+      { lang, pageSize, page: 1 }
     );
     const articles = results.results;
+    const totalPages = results.total_pages;
     return {
+      api,
       articles,
+      totalPages,
     };
   },
+  data() {
+    return {
+      api: null as ResolvedApi | null,
+      articles: [] as Document[],
+      totalPages: 1,
+      page: 1,
+      isLoadingMore: false,
+    };
+  },
+  watch: {
+    '$i18n.locale'() {
+      this.reset();
+    },
+  },
   methods: {
-    getTitle: (article: Document) => {
+    getTitle(article: Document) {
       return PrismicDom.RichText.asText(article.data.article_title);
+    },
+    getSubtitle(article: Document) {
+      return PrismicDom.RichText.asText(article.data.article_subtitle);
+    },
+    reset() {
+      this.articles = [];
+      this.page = 0;
+    },
+    async loadMore(page = 1) {
+      this.isLoadingMore = true;
+      const lang = this.$i18n.locale === 'en' ? 'en-us' : 'fa-ir';
+      if (!this.api) {
+        this.api = await Prismic.getApi(apiUrl);
+      }
+      const results = await this.api.query(
+        Prismic.Predicates.at('document.type', 'articles'),
+        { lang, pageSize, page }
+      );
+      this.page += 1;
+      this.isLoadingMore = false;
+      this.articles = [...this.articles, ...results.results];
+      this.totalPages = results.total_pages;
     },
   },
 });
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .articles {
-  margin: 25px 0;
-  padding: 0 100px;
+  margin-top: 10px;
+}
+.article {
+  margin-top: 12px;
+  margin-bottom: 54px;
   width: 100%;
   display: flex;
   justify-content: center;
   flex-direction: column;
   align-items: center;
+  @include breakpoint(md, max) {
+    margin-top: 10px;
+    margin-bottom: 22px;
+  }
+  @include breakpoint(xs, max) {
+    margin-top: 8px;
+    margin-bottom: 16px;
+  }
 }
-.title {
-  margin: 50px 0;
+
+footer {
+  margin: 90px 0;
+  width: 100%;
+  text-align: center;
+  @include breakpoint(xs, max) {
+    margin: 45px 0;
+  }
 }
-p {
-  color: #000;
-  margin: 15px 0 5px;
-  max-width: 450px;
-  line-height: 1.44;
+
+.done {
+  color: var(--text-light);
 }
 </style>
