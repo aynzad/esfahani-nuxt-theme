52a5d218484d16fcc4819fcf136a0c79a6d92445
Alireza Esfahani <aynzad@gmail.com>
2021-03-28 12:09:23 +0430

    ✨ Add core components

---
+++ components/core/Button.vue
@@ -0,0 +1,137 @@
+<template>
+  <button
+    class="logo"
+    :class="{
+      disabled,
+      loading,
+      [`size-${size}`]: true,
+      [className]: className !== '',
+    }"
+    :disabled="isDisabled"
+    @click="onClick"
+  >
+    <span>
+      <slot v-if="!loading" />
+      <div v-else class="loading-wrapper"><loader /></div>
+    </span>
+  </button>
+</template>
+
+<script lang="ts">
+import Vue, { PropOptions } from 'vue';
+import Loader from '@/components/core/Loader.vue';
+
+type ButtonSize = 'small' | 'medium' | 'big';
+
+export default Vue.extend({
+  name: 'Button',
+  components: { Loader },
+  props: {
+    className: {
+      type: String,
+      default: '',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    size: {
+      type: String,
+      default: 'medium',
+    } as PropOptions<ButtonSize>,
+  },
+  computed: {
+    isDisabled() {
+      return this.loading || this.disabled;
+    },
+  },
+  methods: {
+    onClick(event: MouseEvent) {
+      this.$emit('click', event);
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+button {
+  border: 1px solid var(--button-border-color);
+  color: var(--border-color);
+  text-align: center;
+  outline: none;
+  cursor: pointer;
+  font-family: var(--main-font);
+  position: relative;
+  &:after {
+    position: absolute;
+    transition: transform 0.3s ease-in;
+    transform: translate(0, 0);
+    top: 0px;
+    left: 0px;
+    content: '';
+    width: 100%;
+    height: 100%;
+    display: inline-block;
+    border: 1px solid var(--button-border-color);
+    z-index: -1;
+  }
+  &:hover:after {
+    transition: transform 0.3s ease-out;
+    transform: translate(-4px, -4px);
+  }
+}
+
+button.disabled,
+button.loading {
+  cursor: not-allowed;
+  border: 1px solid var(--button-disabled-border-color);
+  &:after {
+    display: none;
+  }
+}
+
+button.size-small.loading .loading-wrapper {
+  margin-top: -2px;
+}
+button.size-medium.loading .loading-wrapper {
+  margin-top: 6px;
+}
+button.size-big.loading .loading-wrapper {
+  margin-top: 10px;
+}
+
+@include lang(fa) {
+  button:hover:after {
+    transition: transform 0.3s ease-out;
+    transform: translate(4px, -4px);
+  }
+}
+.size-small {
+  height: 40px;
+  min-width: 200px;
+  & > span {
+    font-size: 14px;
+    line-height: 40px;
+  }
+}
+.size-medium {
+  height: 60px;
+  min-width: 260px;
+  & > span {
+    font-size: 20px;
+    line-height: 60px;
+  }
+}
+.size-big {
+  height: 80px;
+  min-width: 320px;
+  & > span {
+    font-size: 26px;
+    line-height: 80px;
+  }
+}
+</style>
---
+++ components/core/Loader.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="loadingio-spinner-ripple">
+    <div class="ldio-odeklonaoi">
+      <div />
+      <div />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Loader',
+  props: {},
+  computed: {},
+  methods: {},
+});
+</script>
+<style scoped>
+@keyframes ldio-odeklonaoi {
+  0% {
+    top: 52px;
+    left: 52px;
+    width: 0;
+    height: 0;
+    opacity: 1;
+  }
+  100% {
+    top: 13px;
+    left: 13px;
+    width: 78px;
+    height: 78px;
+    opacity: 0;
+  }
+}
+.ldio-odeklonaoi div {
+  position: absolute;
+  border-width: 2px;
+  border-style: solid;
+  opacity: 1;
+  border-radius: 50%;
+  animation: ldio-odeklonaoi 0.8695652173913042s cubic-bezier(0, 0.2, 0.8, 1)
+    infinite;
+}
+.ldio-odeklonaoi div:nth-child(1) {
+  border-color: var(--text-main);
+  animation-delay: 0s;
+}
+.ldio-odeklonaoi div:nth-child(2) {
+  border-color: var(--text-light);
+  animation-delay: -0.4347826086956521s;
+}
+.loadingio-spinner-ripple {
+  width: 38px;
+  height: 38px;
+  display: inline-block;
+  overflow: hidden;
+  background: none;
+}
+.ldio-odeklonaoi {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  transform: translateZ(0) scale(0.38);
+  backface-visibility: hidden;
+  transform-origin: 0 0; /* see note above */
+}
+.ldio-odeklonaoi div {
+  box-sizing: content-box;
+}
+</style>
+</style>
