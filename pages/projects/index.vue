555b46b9cbf636a7b1ce285370fe92187377fd4a
Alireza Esfahani <aynzad@gmail.com>
2021-04-23 14:19:14 +0430

    ✨ Add under construction page

---
+++ pages/projects/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="projects">
+    <construction />
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Construction from '~/components/core/construction.vue';
+
+export default Vue.extend({
+  name: 'Projects',
+  components: {
+    Construction,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
